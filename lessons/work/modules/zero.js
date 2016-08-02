e216ee5d93fbb12b2121e790648def6f2cb2e140
qcgm1978 <zhanghl@strongunion.com.cn>
2016-08-02 11:01:04 +0800

    no comment

--- lessons/work/modules/zero.js
+++ lessons/work/modules/zero.js
@@ -2,10 +2,45 @@ require('./styles/zero.scss')
 import React from 'react'
 
 export default React.createClass({
+    getEles: function (data, isAnimate) {
+        let eles = [], style = {}
+        for (let i = 0; i < data.length; i++) {
+            let item = data[i]
+            var left = item.nodeProperties.left;
+            var top = item.nodeProperties.top;
+            var animate = data[i].animate;
+            if (!isAnimate) {
+                style = {
+                    position: 'absolute', left: left + (left.endsWith('%') ? '' : 'px'),
+                    top: top + (top.endsWith('%') ? '' : 'px')
+                };
+            }
+            eles.push(
+                <img src={item.imageSrc} key={i}
+                     id={isAnimate?('word'+i):''}
+                     className={(isAnimate?' animated ':'')+ (animate?animate:'')}
+                     style={ style}/>
+            )
+        }
+        return eles
+    },
+    generateNodesFromJson(){
+        $.getJSON('modules/data/0.json').done((data)=> {
+            let eles = [], animateEles = []
+            eles = this.getEles(data.scenes[0].SceneStaticNodes);
+            animateEles = this.getEles(data.scenes[0].SceneAnimateNodes, true);
+            this.setState({
+                eles: eles,
+                animateEles: animateEles
+            })
+        });
+    },
     getInitialState(){
         return {
             i: 0,
-            frameId: 0
+            frameId: 0,
+            eles: [],
+            animateEles: []
         }
     },
     animate: function (frameId, isMouseEvt) {
@@ -28,33 +63,46 @@ export default React.createClass({
         })
     },
     componentDidMount () {
-        this.animate(this.state.frameId);
-        $('.animated').hover((i, n)=> {
-            this.animate(this.state.frameId, true);
-        })
+        this.generateNodesFromJson()
+
     },
     componentDidUpdate(){
         this.animate(this.state.frameId);
+        $('.animated').off().hover((i, n)=> {
+            this.animate(this.state.frameId, true);
+        })
     },
     render() {
         let classVar = "animated";
         return ( <div id='zero' className='container'>
-                <img id='the' src='./images/0/0-img-the.png'/>
-                <img id='alphabet' src='./images/0/0-img-alphabet.png'/>
-                <img id='dog' src='./images/0/0-img-dog.png'/>
-                <img id='letter' src='./images/0/0-img-letter.png'/>
-                <img id='cat' src='./images/0/0-img-cat.png'/>
-                <img id='tree' src='./images/0/0-img-tree.png'/>
-
+                {this.state.eles.map(function (item, i) {
+                    return item
+                })}
                 <div id='words'>
-                    <img id='word1' className={classVar} src='./images/0/0-word1.png'/>
-                    <img id='word2' className={classVar} src='./images/0/0-word2.png'/>
-                    <img id='word3' className={classVar} src='./images/0/0-word3.png'/>
-                    <img id='word4' className={classVar} src='./images/0/0-word4.png'/>
-                    <img id='word5' className={classVar} src='./images/0/0-word5.png'/>
-
+                    {
+                        this.state.animateEles.map(function (item, i) {
+                            return item
+                        })
+                    }
                 </div>
+                {
 
+                    //<img id='the' src='./images/0/0-img-the.png'/>
+                    //< img id='alphabet' src='./images/0/0-img-alphabet.png'/>
+                    //<img id='dog' src='./images/0/0-img-dog.png'/>
+                    //<img id='letter' src='./images/0/0-img-letter.png'/>
+                    //<img id='cat' src='./images/0/0-img-cat.png'/>
+                    //<img id='tree' src='./images/0/0-img-tree.png'/>
+                    //
+                    //<div id='words'>
+                    //<img id='word1' className={classVar} src='./images/0/0-word1.png'/>
+                    //<img id='word2' className={classVar} src='./images/0/0-word2.png'/>
+                    //<img id='word3' className={classVar} src='./images/0/0-word3.png'/>
+                    //<img id='word4' className={classVar} src='./images/0/0-word4.png'/>
+                    //<img id='word5' className={classVar} src='./images/0/0-word5.png'/>
+                    //
+                    //</div>
+                }
             </div>
         )
     }
