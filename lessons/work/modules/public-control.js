3f8756698891ce93410a7f5f563329c532b2c5b2
qcgm1978 <zhanghl@strongunion.com.cn>
2016-08-08 16:08:32 +0800

    no comment

--- lessons/work/modules/public-control.js
+++ lessons/work/modules/public-control.js
@@ -7,10 +7,10 @@ export default React.createClass({
     getInitialState(){
     },
     componentDidMount () {
-        $.fn.clickToggle = function(func1, func2) {
+        $.fn.clickToggle = function (func1, func2) {
             var funcs = [func1, func2];
             this.data('toggleclicked', 0);
-            this.click(function() {
+            this.click(function () {
                 var data = $(this).data();
                 var tc = data.toggleclicked;
                 $.proxy(funcs[tc], this)();
@@ -18,17 +18,29 @@ export default React.createClass({
             });
             return this;
         };
-        $('.step-show').clickToggle(function () {
-            $(this).css('background-image','url("./images/public-control/step-show/help-click.png")')
-        },function(){
-            $(this).css('background-image','url("./images/public-control/step-show/help.png")')
-
-        })
-        $('.follow-me').clickToggle(function () {
-            $(this).css('background-image','url("./images/public-control/follow-me/follow-click.png")')
-        },function(){
-            $(this).css('background-image','url("./images/public-control/follow-me/follow.png")')
-
+        $('.step-show')
+            .click(function () {
+                $(this)
+                    .find('.check-mark')
+                    .add('.pop-up')
+                    .toggle()
+            })
+            .hover(function () {
+                $(this).css('background-image', 'url("./images/public-control/step-show/help-hover.png")')
+            }, function () {
+                $(this).css('background-image', 'url("./images/public-control/step-show/help.png")')
+            })
+        $('.follow-me')
+            .click(function () {
+                $(this).find('.check-mark').toggle()
+            })
+            .hover(function () {
+                $(this).css('background-image', 'url("./images/public-control/follow-me/follow-me-hover.png")')
+            }, function () {
+                $(this).css('background-image', 'url("./images/public-control/follow-me/follow.png")')
+            })
+        $('.close').click(function () {
+            $('.pop-up').hide()
         })
     },
     componentDidUpdate(){
@@ -43,13 +55,22 @@ export default React.createClass({
             <div id='public-control' className='container'>
                 <div className='indicate'>
                     <div className='step-show'>
-
+                        <div className='check-mark none'></div>
                     </div>
                     <div className='follow-me'>
+                        <div className='check-mark none'></div>
 
                     </div>
                 </div>
-
+                <div className='pop-up none'>
+                    <div className='header'>STEP SHOW
+                        <div className='close'></div>
+                    </div>
+                    <dl>
+                        <dt>STEP 1</dt>
+                        <dd>introduce yourself to the students</dd>
+                    </dl>
+                </div>
             </div>
         )
     }
